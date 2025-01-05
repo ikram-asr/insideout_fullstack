23e917d2c935ffaed69f0d73237f3dc25449a149
oumaima2503 <oumaimahandi2503@gmail.com>
2025-01-05 15:56:40 +0100

    test

--- insideout_be/routes/web.php
+++ insideout_be/routes/web.php
@@ -3,6 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
+
+Route::get('/hello', function () {
+    return response()->json(['message' => 'Hello, world!']);
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
