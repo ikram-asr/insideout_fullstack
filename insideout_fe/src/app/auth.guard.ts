0f09b5baec17c26a2880c333aad5171c1c783b5c
Ikram <ikramassiri9@gmail.com>
2025-01-13 08:26:49 +0100

    mab9ina fahmin walo

--- insideout_fe/src/app/auth.guard.ts
+++ insideout_fe/src/app/auth.guard.ts
@@ -1,25 +1,34 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from './services/authService/auth.service';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
 
-  constructor(private router: Router) {}
+  constructor(private authService: AuthService, private router: Router) { }
 
   canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot
-  ): boolean {
-    const token = localStorage.getItem('token');
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
     
-    if (!token) {
-      // Si le token est absent, rediriger vers la page de connexion
+    // Vérifiez si l'utilisateur est connecté
+    if (this.authService.isAuthenticated()) {
+      const userId = this.authService.getUserId(); // Méthode pour obtenir l'ID de l'utilisateur connecté
+      const routeUserId = Number(next.paramMap.get('id')); // Récupère l'ID utilisateur depuis l'URL
+
+      if (userId === routeUserId) {
+        return true; // Autorise l'accès si les IDs correspondent
+      } else {
+        this.router.navigate(['/']); // Redirige vers une page d'accès refusé
+        return false; // Bloque l'accès si les IDs ne correspondent pas
+      }
+    } else {
+      // Redirection vers la page de connexion si non authentifié
       this.router.navigate(['/login']);
       return false;
     }
-    
-    return true;
   }
 }
